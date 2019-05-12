47002c7c58ab927a4affc94f36f20958282acd7f
muhammadwasi <muhammadwasi228@ymail.com>
2019-05-13 01:24:47 +0500

    Adding project initial files

---
+++ backend/main.py
@@ -0,0 +1,8 @@
+import flask
+
+app=flask.Flask("__main__")
+
+@app.route('/')
+def myIndex():
+    return flask.render_template("index.html")
+app.run(debug=True)
---
+++ backend/static/react/precache-manifest.052e3f0af6283a591cbfe13c8f1bebcd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c51a4ddf749ac32f43ac67faddc3e5df",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "27d9dcd38849a5e823f4",
+    "url": "/static/react/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "44eef4e4e9ab30c1b0c0",
+    "url": "/static/react/css/main.82cf226c.chunk.css"
+  },
+  {
+    "revision": "27d9dcd38849a5e823f4",
+    "url": "/static/react/js/2.63cadbb9.chunk.js"
+  },
+  {
+    "revision": "44eef4e4e9ab30c1b0c0",
+    "url": "/static/react/js/main.81c3c125.chunk.js"
+  },
+  {
+    "revision": "53eac5d40d2daff628d2",
+    "url": "/static/react/js/runtime~main.5d7886d3.js"
+  }
+]);
